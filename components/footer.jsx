46689533cd5011f72f7e5a16c1476ff28b170e86
joel-kalema <joelkalema63@gmail.com>
2024-12-31 01:50:02 +0200

    updated srolle animation

--- components/footer.jsx
+++ components/footer.jsx
@@ -13,14 +13,14 @@ const links = [
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
-        <div className='relative overflow-hidden border-t'>
+        <div className='relative overflow-hidden border-t border-t-[#5757578f]'>
             <h1 className='text-[3rem] lg:text-[14rem] text-center font-semibold mb-2 text-border '>joel  kalema</h1>
-            <div className='flex justify-between px-4 gap-3 mb-4 lg:mb-0 lg:w-5/6 mx-auto'>
+            <div className='flex justify-between px-4 gap-3 mb-4 lg:mb-10 lg:w-4/6 mx-auto'>
                     {links.map((link, index) => (
                         <Link key={index} to={link.idLink} className="cursor-pointer" offset={100} duration={1000}>[ {link.name} ]</Link>
                     ))}
                 </div>
-            <div className='border-t px-2 lg:px-16 py-4 flex flex-col-reverse items-center justify-center lg:justify-between text-[#575757]'>
+            <div className='border-t border-t-[#5757578f] px-2 lg:px-16 py-4 flex flex-col-reverse items-center justify-center lg:justify-between text-[#575757]'>
                 <div
                     variant="small"
                     className="mb-4  font-normal md:mb-0"
