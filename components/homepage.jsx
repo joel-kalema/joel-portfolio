366179d97aec8de03b10ab3303dcfa5626b245b9
joel-kalema <joelkalema63@gmail.com>
2025-01-03 17:01:42 +0200

    updated the design

--- components/homepage.jsx
+++ components/homepage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
                     </div>
                     <div className='lg:flex justify-between items-center'>
                         <p className='lg:w-2/6 mb-4 lg:mb-0 text-[#8f8f8f]'>Let&apos;s make somthing new, different and more meanigful or make think more visual or Conceptual?</p>
-                        <Link to='contact' className='text-[#8f8f8f] border py-2 px-6 rounded-3xl cursor-pointer' offset={100} duration={1000}>Get in Touch!</Link>
+                        <Link to='contact' className='text-[#8f8f8f] border py-2 px-6 rounded-3xl cursor-pointer w-1/2 lg:w-1/6' offset={100} duration={1000}>Get in Touch!</Link>
                     </div>
                 </div>
             </div>
