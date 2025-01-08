29f8b805302b1fc4e15bccb14e80a890e373d574
joel-kalema <joelkalema63@gmail.com>
2025-01-08 13:21:50 +0200

    updated design

--- utils/projects.jsx
+++ utils/projects.jsx
@@ -67,6 +67,28 @@ export const projects = [
     },
     {
         id: 4,
+        image: '/images/neutral.png',
+        images: ['/images/neutral.png', '/images/neutral2.png', '/images/neutral3.png', '/images/neutral4.png'],
+        title: 'Neutral',
+        overview: "Neutral is a digital agency platform offering software development services to businesses, specializing in mobile and web applications.",
+        features: [
+            "Collaboration with over 10+ businesses in the Democratic Republic of Congo (DRC).",
+            "Custom software solutions for mobile and web platforms.",
+            "End-to-end project management services."
+        ],
+        userImpact: "Streamlined digital solutions for clients, enhancing their operational efficiency and customer engagement.",
+        contributions: [
+            "Developed and maintained scalable web and mobile applications.",
+            "Collaborated with stakeholders to define requirements and deliver tailored solutions.",
+            "Optimized applications for performance and usability."
+        ],
+        technologies: ['React', 'Rails', 'SCSS', 'Tailwind'],
+        live: 'https://neutral.codes/',
+        state: 'private',
+        outcome: "Delivered multiple successful projects, improving business processes and customer satisfaction."
+    },
+    {
+        id: 5,
         image: '/images/updev.png',
         images: ['/images/updev.png', '/images/updev2.png', '/images/updev3.png', '/images/updev4.png'],
         title: 'UpDev',
@@ -89,7 +111,7 @@ export const projects = [
         outcome: "Increased user engagement by 40% through interactive and personalized features."
     },
     {
-        id: 5,
+        id: 6,
         image: '/images/eleka.png',
         images: ['/images/eleka.png', '/images/eleka2.png', '/images/eleka3.png', '/images/eleka4.png'],
         title: 'Eleka',
@@ -109,28 +131,6 @@ export const projects = [
         live: 'https://eleka-mvp.vercel.app/',
         state: 'private',
         outcome: "Helped hundreds of users define their career goals and access tailored learning resources."
-    },
-    {
-        id: 6,
-        image: '/images/neutral.png',
-        images: ['/images/neutral.png', '/images/neutral2.png', '/images/neutral3.png', '/images/neutral4.png'],
-        title: 'Neutral',
-        overview: "Neutral is a digital agency platform offering software development services to businesses, specializing in mobile and web applications.",
-        features: [
-            "Collaboration with over 10+ businesses in the Democratic Republic of Congo (DRC).",
-            "Custom software solutions for mobile and web platforms.",
-            "End-to-end project management services."
-        ],
-        userImpact: "Streamlined digital solutions for clients, enhancing their operational efficiency and customer engagement.",
-        contributions: [
-            "Developed and maintained scalable web and mobile applications.",
-            "Collaborated with stakeholders to define requirements and deliver tailored solutions.",
-            "Optimized applications for performance and usability."
-        ],
-        technologies: ['React', 'Rails', 'SCSS', 'Tailwind'],
-        live: 'https://neutral.codes/',
-        state: 'private',
-        outcome: "Delivered multiple successful projects, improving business processes and customer satisfaction."
     }
 ];
 
