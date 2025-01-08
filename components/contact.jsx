c07a97319b800a6c15185444d93b3a66274e585f
joel-kalema <joelkalema63@gmail.com>
2025-01-08 13:34:35 +0200

    updated design

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import { Link } from "next-scroll";
 import React, { useState } from "react";
 import { CiMail } from "react-icons/ci";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
